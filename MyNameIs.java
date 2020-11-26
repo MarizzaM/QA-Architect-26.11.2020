1ef31f7d0f92185ab81eab0fe403ed077fc3e300
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2020-11-26 20:11:49 +0200

    Create MyNameIs.java

---
+++ MyNameIs.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+        String first_name = "Marishka";
+        String last_name = "Pavlova";
+        System.out.println("My name is " + first_name+ " " +last_name);
+    }
+}
